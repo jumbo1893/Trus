b74e85b818c73b3f2b0bbeaf4ddf73f35c905ea4
jumbo1893 <matej.jandak@seznam.cz>
2021-08-18 18:50:07 +0200

    RandomFacts za pokuty přidány, přidán dialog pro zobrazení všech zajímavostí

---
+++ app/src/main/java/com/jumbo/trus/home/FactDialog.java
@@ -0,0 +1,89 @@
+package com.jumbo.trus.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jumbo.trus.Flag;
+import com.jumbo.trus.Model;
+import com.jumbo.trus.OnListListener;
+import com.jumbo.trus.R;
+import com.jumbo.trus.SimpleDividerItemDecoration;
+import com.jumbo.trus.adapters.FinesPlayerNumberRecycleViewAdapter;
+import com.jumbo.trus.adapters.SimpleRecycleViewAdapter;
+import com.jumbo.trus.adapters.StringRecycleViewAdapter;
+import com.jumbo.trus.fine.ReceivedFine;
+import com.jumbo.trus.match.Match;
+import com.jumbo.trus.player.Player;
+import com.jumbo.trus.statistics.StatisticsViewModel;
+
+import java.util.List;
+
+public class FactDialog extends DialogFragment implements View.OnClickListener, OnListListener {
+
+    private static final String TAG = "FactDialog";
+
+    //widgety
+    private Button btn_commit;
+    private RecyclerView rc_list;
+
+    private List<String> factList;
+
+    private StringRecycleViewAdapter stringRecycleViewAdapter;
+
+
+    public FactDialog(List<String> factList) {
+        this.factList = factList;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_fact, container, false);
+        btn_commit = view.findViewById(R.id.btn_commit);
+        rc_list = view.findViewById(R.id.rc_list);
+        rc_list.addItemDecoration(new SimpleDividerItemDecoration(getActivity()));
+        btn_commit.setOnClickListener(this);
+        initFinesRecycleView();
+        setAdapter();
+        return view;
+    }
+
+
+    private void initFinesRecycleView() {
+        Log.d(TAG, "initFinesRecycleView: " + factList.size());
+        stringRecycleViewAdapter = new StringRecycleViewAdapter(factList, getActivity(), this);
+    }
+
+    private void setAdapter() {
+        if (stringRecycleViewAdapter != null) {
+            rc_list.setAdapter(stringRecycleViewAdapter);
+        }
+        rc_list.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_commit: {
+                getDialog().dismiss();
+            }
+        }
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        //dialog_fine_statistics_detail
+    }
+}
